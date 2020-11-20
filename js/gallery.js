215506235df6cf87a684677d4b82bd69a6fb3f65
dapirra <35818038+dapirra@users.noreply.github.com>
2020-11-20 17:06:39 -0500

    Pressing Esc will now close the gallery
    Added some comments

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,30 @@
 window.onload = function () {
+	// Variables
 	const page_container = document.getElementById('page_container');
 	const image_container = document.getElementById('image_container');
 	const full_image = document.getElementById('full_image');
 	let isImageShown = false;
 
+	function gallery_close() {
+		image_container.style.visibility = 'hidden';
+		page_container.style.filter = 'brightness(1)';
+		isImageShown = false;
+	}
+
 	document.body.onclick = function () { // Close the image when clicking away
 		if (isImageShown) {
-			image_container.style.visibility = 'hidden';
-			page_container.style.filter = 'brightness(1)';
-			isImageShown = false;
+			gallery_close();
 		}
 	};
 
-	// Stop the image from closing when clicking on it
+	document.body.onkeyup = function (e) { // Close the image when Esc is pressed
+		if (isImageShown && e.key === 'Escape') {
+			gallery_close();
+		}
+	};
+
 	full_image.onclick = function (e) {
-		e.stopPropagation();
+		e.stopPropagation(); // Stop the image from closing when clicking on it
 	};
 
 	document.getElementById('image_gallery').childNodes.forEach(function (image_link) {
