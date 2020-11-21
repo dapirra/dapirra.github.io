081cf501c5b20959a8111340de34d8281d7dd27a
dapirra <35818038+dapirra@users.noreply.github.com>
2020-11-21 18:56:58 -0500

    Added the ability to switch to the next or previous image

--- js/gallery.js
+++ js/gallery.js
@@ -4,11 +4,24 @@ window.onload = function () {
 	const image_container = document.getElementById('image_container');
 	const full_image = document.getElementById('full_image');
 	let isImageShown = false;
+	HTMLCollectionToArray = x => [].slice.call(x);
+	all_image_links = [];
+	currentImage = 0;
 
 	function gallery_close() {
 		image_container.style.visibility = 'hidden';
 		page_container.style.filter = 'brightness(1)';
 		isImageShown = false;
+	}
+
+	function gallery_prev() {
+		if (currentImage == 0) return;
+		full_image.src = all_image_links[--currentImage];
+	}
+
+	function gallery_next() {
+		if (currentImage == all_image_links.length - 1) return;
+		full_image.src = all_image_links[++currentImage];
 	}
 
 	document.body.onclick = function () { // Close the image when clicking away
@@ -18,24 +31,42 @@ window.onload = function () {
 	};
 
 	document.body.onkeyup = function (e) { // Close the image when Esc is pressed
-		if (isImageShown && e.key === 'Escape') {
-			gallery_close();
+		if (isImageShown) {
+			switch (e.key) {
+				case 'Escape':
+					gallery_close();
+					break;
+				case 'ArrowLeft':
+					gallery_prev();
+					break;
+				case 'ArrowRight':
+					gallery_next();
+			}
 		}
 	};
 
 	full_image.onclick = function (e) {
 		e.stopPropagation(); // Stop the image from closing when clicking on it
+		if (e.clientX < window.innerWidth / 2) {
+			gallery_prev();
+		} else {
+			gallery_next();
+		}
 	};
 
-	document.getElementById('image_gallery').childNodes.forEach(function (image_link) {
+	HTMLCollectionToArray(document.getElementById('image_gallery').children).forEach(function (image_link, index) {
+		image_link.setAttribute('index', index);
+		all_image_links.push(image_link.href);
+
 		image_link.onclick = function (e) {
 			e.preventDefault();
 			if (!isImageShown) {
 				e.stopPropagation();
 				page_container.style.filter = 'brightness(0.5)';
-				full_image.src = image_link.href;
+				full_image.src = e.currentTarget.href;
 				image_container.style.visibility = 'visible';
 				isImageShown = true;
+				currentImage = Number(e.currentTarget.getAttribute('index'));
 			}
 		};
 	});
