4d31b36a00a7230f51a9bad8c4c170f1aa40458e
dapirra <35818038+dapirra@users.noreply.github.com>
2021-01-20 18:49:36 -0500

    Improved 404 page code by using a dictionary instead of if statements

--- js/404.js
+++ js/404.js
@@ -18,51 +18,43 @@ const procedurally_generated_forest = 'procedurally_generated_forest.html';
 const filename_fixer = 'filename_fixer.html';
 const mips_happy_birthday = 'mips_happy_birthday.html';
 
-function guessPage(url) {
-	if (url.includes('flash')) {
-		if (url.includes('mock')) {
-			return flashcards_mockup;
-		} else {
-			return flashcards_app;
+directory = {
+	'flash': {
+		'mock': flashcards_mockup,
+		'': flashcards_app
+	},
+	'pack': packpackmonsters,
+	'cpu': cpu_simulator,
+	'mem': {
+		'sim': memory_simulator,
+		'iphone': iphone_memory_game,
+		'game': iphone_memory_game,
+		'': memory_simulator,
+	},
+	'mine': minesweeper,
+	'iphone': iphone_memory_game,
+	'temp': tempbot,
+	'png': apng_thing,
+	'multi': autoit_multiprocessing,
+	'uniform': uniform_generator,
+	'mock': flashcards_mockup,
+	'forest': procedurally_generated_forest,
+	'file': filename_fixer,
+	'fix': filename_fixer,
+	'happy': mips_happy_birthday,
+	'birth': mips_happy_birthday
+}
+
+function guessPageDict(url, dir) {
+	for (item in dir) {
+		if (url.includes(item, 1)) {
+			return typeof dir[item] === 'string' ? dir[item] : guessPageDict(url, dir[item])
 		}
-	} else if (url.includes('pack')) {
-		return packpackmonsters;
-	} else if (url.includes('cpu')) {
-		return cpu_simulator;
-	} else if (url.includes('mem')) {
-		if (url.includes('sim')) {
-			return memory_simulator;
-		} else if (url.includes('iphone') ||
-					url.includes('game')) {
-			return iphone_memory_game;
-		} else {
-			return memory_simulator;
-		}
-	} else if (url.includes('mine')) {
-		return minesweeper;
-	} else if (url.includes('iphone')){
-		return iphone_memory_game;
-	} else if (url.includes('temp')) {
-		return tempbot;
-	} else if (url.includes('png')) {
-		return apng_thing;
-	} else if (url.includes('autoit') && url.includes('multi')) {
-		return autoit_multiprocessing;
-	} else if (url.includes('uniform')) {
-		return uniform_generator;
-	} else if (url.includes('mock')) {
-		return flashcards_mockup;
-	} else if (url.includes('forest')) {
-		return procedurally_generated_forest;
-	} else if (url.includes('file') || url.includes('fix')) {
-		return filename_fixer;
-	} else if (url.includes('happy') || url.includes('birth')) {
-		return mips_happy_birthday;
 	}
 }
 
 if (!location.pathname.startsWith('/404')) {
-	var page = guessPage(location.pathname);
+	var page = guessPageDict(location.pathname, directory);
 	if (page !== undefined) {
 		location.replace(page);
 	}
