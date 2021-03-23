00c494a9b448bddce684895e03b035afd39175c3
dapirra <35818038+dapirra@users.noreply.github.com>
2021-03-23 02:27:00 -0400

    The back button will now properly go back when pressed

---
+++ _js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+	var back_button = document.getElementById('back_button');
+	back_button.onclick = function(e) {
+		e.preventDefault();
+		if (document.referrer === 'https://dapirra.github.io/' ||
+		document.referrer.includes('127.0.0.1')) {
+			history.back();
+		} else {
+			location.href = '/';
+		}
+		return false;
+	}
+}, false);
