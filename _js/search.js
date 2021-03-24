cc4fd1ddbffc7a5d8596873ad224d45db96c95b5
dapirra <35818038+dapirra@users.noreply.github.com>
2021-03-24 04:39:06 -0400

    Added a clear search button and made some optimizations to the search

--- _js/search.js
+++ _js/search.js
@@ -1,13 +1,21 @@
 window.addEventListener('load', function() {
 	var search_bar = document.getElementById('search_bar');
+	var search_clear = document.getElementById('search_clear');
+	section_titles = document.querySelectorAll('.section_title');
 	tiles = document.querySelectorAll('.card');
 	search_bar.oninput = function() {
 		filterTiles(search_bar.value);
 	}
+	search_clear.onclick = function() {
+		search_bar.value = '';
+		filterTiles('');
+	}
+
 }, false);
 
 function filterTiles(searchText) {
 	searchText = searchText.trim().toLowerCase();
+
 	for (const tile of tiles) {
 		var tileTitle = tile.firstElementChild.firstElementChild.textContent.toLowerCase();
 		var tileParent = tile.parentElement;
@@ -16,14 +24,15 @@ function filterTiles(searchText) {
 		} else {
 			tileParent.classList.add('hidden');
 		}
-		if (searchText === '') {
-			document.querySelectorAll('.section_title').forEach(function (element) {
-				element.classList.remove('hidden');
-			});
-		} else {
-			document.querySelectorAll('.section_title').forEach(function (element) {
-				element.classList.add('hidden');
-			});
+	}
+
+	if (searchText === '') {
+		for (const element of section_titles) {
+			element.classList.remove('hidden');
+		}
+	} else {
+		for (const element of section_titles) {
+			element.classList.add('hidden');
 		}
 	}
 }
