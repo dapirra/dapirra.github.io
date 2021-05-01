18f23211ae3335b4b60cbd6044cfa5093452c1e9
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-01 05:01:36 -0400

    feat: Added tags to each of the pages

--- _js/search.js
+++ _js/search.js
@@ -3,16 +3,30 @@ window.addEventListener('load', function() {
 	var search_clear = document.getElementById('search_clear');
 	section_titles = document.querySelectorAll('.section_title');
 	tiles = document.querySelectorAll('#homepage_content_container > a');
+
 	search_bar.oninput = function() {
 		filterTiles(search_bar.value);
 	}
+
 	search_clear.onclick = function() {
 		search_bar.value = '';
 		filterTiles('');
+
+		// Remove the # from the URL without reloading
+		// https://stackoverflow.com/a/15323220
+		if (location.href.lastIndexOf('#')) {
+			history.replaceState('', document.title, '/');
+		}
 	}
 }, false);
 
 window.onpageshow = function() {
+	// Place the text from the hash into the searchbar if it starts with #$
+	if (location.hash.startsWith('#$')) {
+		search_bar.value = decodeURI(location.hash.substr(2));
+	}
+
+	// Search for a previous value left in the searchbar if there is one
 	if (search_bar.value !== '') {
 		filterTiles(search_bar.value);
 	}
